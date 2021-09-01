a62b00a23fd06c907f6e484bb219e7957711feca
RadoMiami <kguthre@gmail.com>
2021-09-01 12:18:11 -0400

    Initial commit

---
+++ src/main/java/Assignment/Solution01.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Solution01 {
+
+    public static void main(String[] args) {
+        /*
+         *  UCF COP3330 Fall 2021 Assignment 1 Solution
+         *  Copyright 2021 first_name last_name
+         */
+        /* Make a scanner,
+        then ask name,
+        then create variable for name and store it,
+        and combine string
+        and greet! */
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = in.nextLine();
+        String lastString = "Hello, " + name + ", nice to meet you!";
+        System.out.println(lastString);
+    }
+}
